76da70373c739a1c2d4f4254637f899125e6e58b
Ccode-lang <coop112097@gmail.com>
2023-12-07 20:58:15 -0700

    Add project files.

---
+++ Patches/Player/SpeedPatches.cs
@@ -0,0 +1,19 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMovement.Patches.Player
+{
+    [HarmonyPatch(typeof(PlayerControllerB), "Start")]
+    internal class SpeedPatches
+    {
+        public static void Prefix(PlayerControllerB __instance)
+        {
+            __instance.movementSpeed = 20;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMovement
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.FastMovement";
+        private const string PluginName = "FastMovement";
+        private const string VersionString = "0.0.1";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
